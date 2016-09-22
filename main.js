11fa3b3bde6ba38b1a30637b2ae8e2dcd9942cb5
Bailey Busch <baileybusch@gmail.com>
2016-09-22 12:06:22 -0700

    js
    js

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-<script>
+
     (function () {
         var t = document.createElement('script');
-        t.setAttribute("data-cordial-track-key", "$sandboxce1");
+        t.setAttribute("data-cordial-track-key", "sandboxce1");
         t.setAttribute("data-cordial-url", "track.cordial.io");
         t.setAttribute("data-auto-track", true);
         t.src = 'https://track.cordial.io/track.js';
         t.async = true;
         document.body.appendChild(t);
     })();
-</script>
+
 
