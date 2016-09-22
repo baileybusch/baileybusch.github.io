934711f89e5234a5abfa9febff01db9a08502f50
Bailey Busch <baileybusch@gmail.com>
2016-09-21 17:15:10 -0700

    updated
    updated account

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 <script>
     (function () {
         var t = document.createElement('script');
-        t.setAttribute("data-cordial-track-key", "$accountkey");
+        t.setAttribute("data-cordial-track-key", "$sandboxce1");
         t.setAttribute("data-cordial-url", "track.cordial.io");
         t.setAttribute("data-auto-track", false);
         t.src = 'https://track.cordial.io/track.js';
