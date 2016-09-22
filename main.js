c20680f68d1153924686147bab87f26e185e8f24
Bailey Busch <baileybusch@gmail.com>
2016-09-22 11:30:52 -0700

    script update
    update

--- main.js
+++ main.js
@@ -3,9 +3,10 @@
         var t = document.createElement('script');
         t.setAttribute("data-cordial-track-key", "$sandboxce1");
         t.setAttribute("data-cordial-url", "track.cordial.io");
-        t.setAttribute("data-auto-track", false);
+        t.setAttribute("data-auto-track", true);
         t.src = 'https://track.cordial.io/track.js';
         t.async = true;
         document.body.appendChild(t);
     })();
 </script>
+
