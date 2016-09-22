fe846dd5423f1c017ba7670340f8db20eb385e1a
Bailey Busch <baileybusch@gmail.com>
2016-09-21 17:13:15 -0700

    added JS script
    JS script

---
+++ main.js
@@ -0,0 +1,11 @@
+<script>
+    (function () {
+        var t = document.createElement('script');
+        t.setAttribute("data-cordial-track-key", "$accountkey");
+        t.setAttribute("data-cordial-url", "track.cordial.io");
+        t.setAttribute("data-auto-track", false);
+        t.src = 'https://track.cordial.io/track.js';
+        t.async = true;
+        document.body.appendChild(t);
+    })();
+</script>
